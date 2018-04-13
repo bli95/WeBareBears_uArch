57625d32fc405464e76a76effc73aade5037c8c6
Harsh Modhera <hkmodhera@gmail.com>
2018-04-13 13:13:49 -0500

    keeping files as backup

--- srccode/write_buffer.sv
+++ srccode/write_buffer.sv
@@ -2,52 +2,42 @@ import lc3b_types::*;
 
 module write_buffer
 (
-	input logic [127:0] wdata,
-	input logic [11:0] w_address,
-	input logic mem_ack, w_req, 
+	input clk,
+	input logic [127:0] w_data_in,
+	input logic [11:0] w_addr_in,
+	input logic mem_ack, L2_req, 
 	
-	output logic EWB_ack,
+	output logic [127:0] w_data_out,
+	output logic [11:0] w_addr_out,
+	output logic EWB_ack, EWB_req
 );
 
-	logic [127:0] data_1;
-	logic [11:0] addr_1;
-	logic valid_1, load_1;
+	logic valid_bit, load_v, load_d;
 	
-	enum int unsigned {hold_nbd, hold_sbd, mem_write, mem_break} state, next_state;
+	enum int unsigned {hold_nbd, hold_sbd, mem_write, mem_break_1, mem_break_2} state, next_state;
 	
 	always_comb begin
 		
-		load_1 = 1'b0;
-		EWB_data_found = 1'b0;
-	
+		load_v = 1'b0;
+		load_d = 1'b0;
+		EWB_ack = 1'b0;
+			
 		case(state)
 			hold_nbd:
 			begin
-				if (w_req) begin
-					load_1 = 1'b1;
+				if (L2_req) begin
+					load_v = 1'b1;
+					load_d = 1'b1;
 					EWB_ack = 1'b1;
 				end
-				else begin
-					EWB_ack = 1'b0;
-				end
-			end
-			hold_sbd:
-			begin
-				EWB_ack = 1'b0;
-			end
+			end	
 			mem_write:
 			begin
 				if (mem_ack) begin
-					EWB_ack = 1'b1;
-				end
-				else begin
-					EWB_ack = 1'b0;
+					load_v = 1;
 				end
 			end
-			mem_break:
-			begin
-				EWB_ack = 1'b0;
-			end
+			hold_sbd, mem_break_1, mem_break_2: ;
 		endcase
 	end
 	
@@ -56,42 +46,44 @@ module write_buffer
 		case(state)
 			hold_nbd:
 			begin
-				if (w_req) begin
-					next_state = mem_break;
+				if (L2_req) begin
+					next_state = mem_break_1;
 				end
 				else begin
-					next_state = hold_nbd;
+					next_state = state;
 				end
 			end
 			hold_sbd:
 			begin
 				if (mem_ack) begin
-					next_state = mem_write;
+					next_state = mem_break_2;
 				end
 				else begin
-					next_state = hold_sbd;
+					next_state = state;
 				end
 			end
 			mem_write:
 			begin
 				if (mem_ack) begin
-					next_state = mem_break;
+					next_state = hold_nbd;
 				end
 				else begin
-					next_state = mem_write;
+					next_state = state;
 				end
 			end
-			mem_break:
+			mem_break_1:
 			begin
 				next_state = hold_sbd;
 			end
+			mem_break_2:
+			begin
+				next_state = mem_write;
+			end
 		endcase
 	end
 	
-	register #(.width(1)) EWB_VALID_1 (.clk, .load(load_1), .in(), .out(valid_1));
-	
-	register #(.width(12)) EWB_ADDR_1 (.clk, .load(load_1), .in(w_address), .out(addr_out_1));
-
-	register #(.width(128)) EWB_DATA_1 (.clk, .load(load_1), .in(wdata), .out(data_out_1));
+	register #(.width(1)) EWB_VALID_1 (.clk, .load(load_v), .in(!valid_bit), .out(valid_bit));
+	register #(.width(12)) EWB_ADDR_1 (.clk, .load(load_d), .in(w_addr_in), .out(w_addr_out));
+	register #(.width(128)) EWB_DATA_1 (.clk, .load(load_d), .in(w_data_in), .out(w_data_out));
 
 endmodule : write_buffer
--- testbenches/dcache_phys_mainpc_tb.sv
+++ testbenches/dcache_phys_mainpc_tb.sv
@@ -4,6 +4,9 @@ timeunit 1ns;
 timeprecision 1ns;
 
 logic clk;
+logic EWB_ack, EWB_req;
+logic [11:0] w_addr_out;
+logic [127:0] w_data_out;
 
 /* Clock generator */
 initial clk = 0;
@@ -12,7 +15,8 @@ always #5 clk = ~clk;
 wishbone icache_arbiter(clk);
 wishbone dcache_arbiter(clk);
 wishbone arbiter_L2cache(clk);
-wishbone L2cache_mem(clk);
+wishbone L2cache_EWB(clk);
+wishbone EWB_mem(clk);
 
 mainpc WeBareBears(
 	.ibus(icache_arbiter),
@@ -26,24 +30,34 @@ arbiter sel_cache(
 );
 
 cache L2_cache(
-	.wb(L2cache_mem),
+	.wb(L2cache_EWB),
 	.sb(arbiter_L2cache),
-	.L2_ack,
-	.L2_data
 );
 
-mux2 #(.width(1)) select_ACK (.sel(L2cache_mem.WE), .a(L2cache_mem.ACK), .b(EWB_ack), .z(L2_ack));
+mux2 #(.width(1)) select_ACK (.sel(L2cache_EWB.WE), .a(L2cache_EWB.ACK), .b(EWB_ack), .z(L2cache_EWB.ACK));
 
 write_buffer EWB(
-	.wdata(L2cache_mem.DAT_M),
-	.w_address(L2cache_mem.ADR),
-	.mem_ack(L2cache_mem.ACK),
-	.w_req(L2cache_mem.WE),
+	.w_data_in(L2cache_EWB.DAT_M),
+	.w_addr_in(L2cache_EWB.ADR),
+	.mem_ack(EWB_mem.ACK),
+	.L2_req(L2cache_EWB.WE),
+	.w_data_out,
+	.w_addr_out,
 	.EWB_ack,
+	.EWB_req
 );
 
+EWB_mem.CLK = L2cache_EWB.CLK;
+L2cache_EWB.DAT_S = EWB_mem.DAT_S;
+mux2 #(.width(128)) select_DAT (.sel(EWB_req), .a(L2cache_EWB.DAT_M), .b(w_data_out), .z(EWB_mem.DAT_M))
+mux2 #(.width(12)) select_ADR (.sel(EWB_req), .a(L2cache_EWB.ADR), .b(w_addr_out), .z(EWB_mem.ADR));
+EWB_mem.WE = EWB_req;
+EWB_mem.STB = EWB_req || L2cache_EWB.STB;
+EWB_mem.CYC = EWB_req || L2cache_EWB.CYC;
+EWB_mem.SEL = 16'hffff;
+
 physical_memory mem(
-	.wb(L2cache_mem)
+	.wb(EWB_mem)
 );
 
 endmodule : dcache_phys_mainpc_tb
